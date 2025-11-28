75094bd4a1149b287c9c53eedffab9a2b36543ef
sedathoca <sedatbaskan@gmail.com>
2025-11-29 02:06:06 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,49 +1,10 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Permissions-Policy',
-            value: 'usb=(), serial=(), hid=(), cross-origin-isolated=(), autoplay=()'
-          }
-        ],
-      },
-    ];
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
   },
 };
 
 export default nextConfig;
-
-    
